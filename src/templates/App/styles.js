74f8bf306cf99a373d2a398e4d67765de4682109
Kleiton Albuquerque <kleiton.silva@yduqs.com.br>
2023-06-23 09:51:17 -0300

    Configurando o ThemeProvider do styled-components

--- src/templates/App/styles.js
+++ src/templates/App/styles.js
@@ -1,9 +1,12 @@
 import styled, { css } from "styled-components";
 
-export const Wrapper = styled.h1`
-  ${(props) => css`
+export const Wrapper = styled.div`
+  /* ${(props) => css`
     background: ${props.background};
     color: ${props.color};
     padding: 1rem;
+  `} */
+  ${({ theme }) => css`
+    background: ${theme.colors.secondaryBg};
   `}
 `;
